ad6184f3a36dff4217d1bbe6f27692a00921b590
breno <breno.nunes@segarante.com.br>
2025-04-30 15:23:53 -0300

    final version

---
+++ README.md
@@ -0,0 +1,27 @@
+# caldeirao_do_hook
+
+TODO: Write a description here
+
+## Installation
+
+TODO: Write installation instructions here
+
+## Usage
+
+TODO: Write usage instructions here
+
+## Development
+
+TODO: Write development instructions here
+
+## Contributing
+
+1. Fork it (<https://github.com/your-github-user/caldeirao_do_hook/fork>)
+2. Create your feature branch (`git checkout -b my-new-feature`)
+3. Commit your changes (`git commit -am 'Add some feature'`)
+4. Push to the branch (`git push origin my-new-feature`)
+5. Create a new Pull Request
+
+## Contributors
+
+- [breno](https://github.com/your-github-user) - creator and maintainer
---
+++ src/caldeirao_do_hook.cr
@@ -0,0 +1,53 @@
+require "kemal"
+
+class Hook
+  getter :body, :last_accessed
+  @body : String
+  @last_accessed : Time | Nil
+
+  def initialize(body)
+    @body = body
+    @last_accessed = nil
+  end
+
+  def access!
+    @last_accessed = Time.local
+  end
+end
+
+class HookStore
+  @@hooks = [] of Hook
+
+  def self.save(hook)
+    @@hooks << hook
+  end
+
+  def self.get
+    hooks = @@hooks.select{ |h| h.last_accessed.nil? }
+    hooks.each { |h| h.access! }
+
+    hooks.map { |h| h.body }
+  end
+end
+
+def save_hook(env, from_query = false)
+  if from_query
+    hook = Hook.new(env.params.query.to_s)
+    HookStore.save(hook)
+  else
+    hook = Hook.new(env.params.body.to_s)
+    HookStore.save(hook)
+  end
+end
+
+get("/*") { |env| save_hook(env, from_query: true); "data saved!" }
+post("/*") { |env| save_hook(env); "data saved!" }
+patch("/*") { |env| save_hook(env); "data saved!" }
+delete("/*") { |env| save_hook(env); "data saved!" }
+
+get "/" do |env|
+  env.response.content_type = "application/json"
+  HookStore.get.to_json
+end
+
+Kemal.run
